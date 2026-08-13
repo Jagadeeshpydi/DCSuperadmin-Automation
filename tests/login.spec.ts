eafcd8e217b7cfd315b192e5d9ea0dbc8d327ec1
Jagadeesh Pydi <jagadeeshpydi143@gmail.com>
2026-08-13 17:43:47 +0530

    Add login page POM and test cases

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { Login } from '../pages/login';
 
+
 test.describe('Login Page Test Cases', () => {
 
     test('Verify the login with valid data', async ({ page }) => {
